8722abbb387ee74c36463f83d3bbaf047fa7b63f
Calena <agaru.corp@gmail.com>
2025-07-24 21:54:07 -0400

    LinkedIn link

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -99,7 +99,7 @@ const Index = () => {
               <div className="flex-1 flex flex-col items-start">
                 <h4 className="text-lg font-semibold mb-4">Connect</h4>
                 <div className="flex space-x-4">
-                  <a href="https://linkedin.com/in/blas-chiaretta" target="_blank" rel="noopener noreferrer" className="hover:text-blue-400 transition-colors">
+                  <a href="https://www.linkedin.com/in/blas-chiaretta-20474a5a/" target="_blank" rel="noopener noreferrer" className="hover:text-blue-400 transition-colors">
                     <svg width="24" height="24" fill="currentColor" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-10h3v10zm-1.5-11.268c-.966 0-1.75-.784-1.75-1.75s.784-1.75 1.75-1.75 1.75.784 1.75 1.75-.784 1.75-1.75 1.75zm15.5 11.268h-3v-5.604c0-1.337-.025-3.063-1.868-3.063-1.868 0-2.154 1.459-2.154 2.968v5.699h-3v-10h2.881v1.367h.041c.401-.761 1.379-1.563 2.841-1.563 3.039 0 3.6 2.001 3.6 4.601v5.595z"/></svg>
                   </a>
                   <a href="mailto:blas.chiaretta@gmail.com" className="hover:text-blue-400 transition-colors">
